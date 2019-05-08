3fd30c3aaf6c9279b1461718db1598ced1ed0224
Yejj <1271361907@qq.com>
2019-05-08 22:59:47 +0800

    second

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,16 +6,20 @@
     <el-main>
       <nuxt />
     </el-main>
-    <el-footer  height="100%">Footer</el-footer>
+    <el-footer  height="100%">
+      <my-footer/>
+    </el-footer>
 </el-container>
 
 </template>
 
 <script>
 import MyHeader from '@/components/public/header/index.vue'
+import MyFooter from '@/components/public/footer/index.vue'
 export default {
   components:{
-    MyHeader
+    MyHeader,
+    MyFooter
   }
 }
 </script>
